babd72e38cbeac7f34db450c16e8e83171175915
LeoMezba <csmezba@gmail.com>
2020-12-29 00:34:00 +0600

    Getting places by user id

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -29,24 +29,27 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
-
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator : userId }).exec();
-  }
-  catch (e) {
-    const err = new HttpError('Could Not find a user by id', 500);
-    return next(err);
+    userWithPlaces = await User.findById(userId).populate('places');
+  } catch (err) {
+    const error = new HttpError(
+        'Fetching places failed, please try again later',
+        500
+    );
+    return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
-      new HttpError('Could not find places for the provided user id.', 404)
+        new HttpError('Could not find places for the provided user id.', 404)
     );
   }
 
   res.json({
-    places : places.map( places => places.toObject({ getters : true }) )
+    places: userWithPlaces.places.map(place =>
+        place.toObject({ getters: true })
+    )
   });
 };
 
